a26fe46f0b6dd4c77ab5facdc50bbfb7ab65dfb5
Nova Wolfcreek <novawko@gmail.com>
2025-07-28 23:46:23 -0500

    Add files via upload

---
+++ styles/js/pages/characters.js
@@ -0,0 +1,45 @@
+/* ==================================================================== */
+/* Import Charadex
+======================================================================= */
+import { charadex } from '../charadex.js';
+
+
+/* ==================================================================== */
+/* Load
+======================================================================= */
+document.addEventListener("DOMContentLoaded", async () => {
+
+  let dex = await charadex.initialize.page(
+    null,
+    charadex.page.characters,
+    null, 
+    async (listData) => {
+
+      if (listData.type == 'profile') {
+
+        console.log(listData.profileArray[0]);
+
+        // Create the log dex
+        if (charadex.tools.checkArray(listData.profileArray[0].characterlog)) {
+          let logs = await charadex.initialize.page(
+            listData.profileArray[0].characterlog,
+            charadex.page.characters.relatedData['character log']
+          );
+        }
+
+        // Create the image gallery
+        if (charadex.tools.checkArray(listData.profileArray[0].imagegallery)) {
+          let gallery = await charadex.initialize.page(
+            listData.profileArray[0].imagegallery,
+            charadex.page.characters.relatedData['image gallery']
+          );
+        }
+
+      }
+
+    }
+  );
+  
+  charadex.tools.loadPage('.softload', 500);
+  
+});
