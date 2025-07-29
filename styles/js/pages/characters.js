5453de43fbc9258e149f436612e3fd1320cc41ed
Nova Wolfcreek <novawko@gmail.com>
2025-07-29 02:38:25 -0500

    Update characters.js

--- styles/js/pages/characters.js
+++ styles/js/pages/characters.js
@@ -39,6 +39,37 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     }
   );
+
+/* ==================================================================== */
+/* Spilt them annoying prefixes to look nicer
+======================================================================= */
+if (urlParams.has('id')) {
+        
+        (() => {
+
+          let len = sheetArray.length;
+          while (len--) {
+            if (sheetArray[len].orderID == urlParams.get('id').replace(/\D+/gm,"")) {
+              
+              // Prev
+              if (sheetArray[len - 1]) {
+                $("#entryPrev").attr("href", url.href.split('?id')[0].split('&id')[0] + preParam + sheetArray[len - 1].id);
+                $("#entryPrev span").text(sheetArray[len - 1].id);
+              } else {
+                $("#entryPrev i").remove();
+              }
+
+              // Next
+              if (sheetArray[len + 1]) {
+                console.log(sheetArray[len + 1]);
+                $("#entryNext").attr("href", url.href.split('?id')[0].split('&id')[0] + preParam + sheetArray[len + 1].id);
+                $("#entryNext span").text(sheetArray[len + 1].id);
+              } else {
+                $("#entryNext i").remove();
+              }
+
+            }
+          }
   
   charadex.tools.loadPage('.softload', 500);
   
