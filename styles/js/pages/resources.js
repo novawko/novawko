6e6773df000fb4c0965bf597c7e71dd4232ef9bd
Nova Wolfcreek <novawko@gmail.com>
2025-08-02 23:46:56 -0500

    Update resources.js

--- styles/js/pages/resources.js
+++ styles/js/pages/resources.js
@@ -25,20 +25,6 @@ document.addEventListener("DOMContentLoaded", async () => {
     }
 
   );
-
-/* ==================================================================== */
-/* Load the bookmarks when visited
-======================================================================= */
-      window.addEventListener('load', () => {
-        const targetElement = document.getElementById('bookmarks');
-        if (targetElement) {
-            targetElement.scrollIntoView({
-                behavior: 'smooth', // Optional: for smooth scrolling animation
-                block: 'start'      // Optional: aligns the top of the element with the top of the viewport
-            });
-        }
-    });
-
   
   charadex.tools.loadPage('.softload', 500);
   
