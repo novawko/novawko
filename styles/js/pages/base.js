063359926540e8d0eefb85aa1fac1a93b995caa2
Nova Wolfcreek <novawko@gmail.com>
2025-08-04 06:32:05 -0500

    Update base.js

--- styles/js/pages/base.js
+++ styles/js/pages/base.js
@@ -10,22 +10,5 @@ document.addEventListener("DOMContentLoaded", () => {
   charadex.tools.loadIncludedFiles();
   charadex.tools.updateMeta();
   charadex.tools.loadPage('#charadex-body', 100);
-
-/* ==================================================================== */
-/* Load IDs
-======================================================================= */
-// Get the button element
-const button = document.getElementById("permissions");
-
-// Get the target element
-const targetSection = document.getElementById("permissions");
-
-// Add a click event listener to the button
-button.addEventListener("click", () => {
-  // Scroll the target section into view
-  targetSection.scrollIntoView({
-    block: "start",   // Align the top of the element with the top of the viewport
-  });
-});
   
 });
