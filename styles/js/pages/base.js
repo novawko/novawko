e173f9174b620d2129c7fc16fec619894be0582d
Nova Wolfcreek <novawko@gmail.com>
2025-08-04 05:58:29 -0500

    Update base.js

--- styles/js/pages/base.js
+++ styles/js/pages/base.js
@@ -10,5 +10,14 @@ document.addEventListener("DOMContentLoaded", () => {
   charadex.tools.loadIncludedFiles();
   charadex.tools.updateMeta();
   charadex.tools.loadPage('#charadex-body', 100);
+
+
+/* ==================================================================== */
+/* Load the IDs into view when visited
+======================================================================= */
+  function myFunction() {
+  const element = document.getElementById("permissions");
+  element.scrollIntoView();
+}
   
 });
