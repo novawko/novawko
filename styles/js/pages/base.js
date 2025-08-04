2e2c22d3c9785f8acf776f953ef3746a3597a96c
Nova Wolfcreek <novawko@gmail.com>
2025-08-04 05:36:41 -0500

    Update base.js

--- styles/js/pages/base.js
+++ styles/js/pages/base.js
@@ -10,26 +10,5 @@ document.addEventListener("DOMContentLoaded", () => {
   charadex.tools.loadIncludedFiles();
   charadex.tools.updateMeta();
   charadex.tools.loadPage('#charadex-body', 100);
-
-/* ==================================================================== */
-/* Scroll to the hashtag elements automatically when visiting via address bar
-======================================================================= */
-/* ==================================================================== */
-/* Scroll to the hashtag elements automatically when visiting via address bar
-======================================================================= */
-window.addEventListener('load', () => {
-  // Get the hash from the URL
-  const hash = window.location.hash;
-
-  // Check if a hash exists and if an element with that ID exists
-  if (hash) {
-    const targetElement = document.querySelector(hash); // Selects by ID (e.g., #section-id)
-
-    if (targetElement) {
-      targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' }); 
-    }
-  }
-});
-
   
 });
