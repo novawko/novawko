4b5fb97a22b578e0f2f47c68c4821978ce3254b5
Nova Wolfcreek <novawko@gmail.com>
2025-08-06 02:20:34 -0500

    Update base.js

--- styles/js/pages/base.js
+++ styles/js/pages/base.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
   charadex.tools.updateMeta();
   charadex.tools.loadPage('#charadex-body', 100);
 
-  let designs = arr.slice(-3);
+  let design.slice(-3);
   
 });
 
