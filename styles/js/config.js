f6c411d3fc517b2919893af4fcdd1170e8f12b9c
Nova Wolfcreek <novawko@gmail.com>
2025-08-06 12:26:26 -0500

    Update config.js

--- styles/js/config.js
+++ styles/js/config.js
@@ -382,8 +382,8 @@ charadex.page.characters = {
       ... charadex.page.imageGallery,
 
       sheetPage: charadex.sheet.pages.imageGallery,
-      primaryProperty: 'design',
-      relatedProperty: 'designs',
+      primaryProperty: 'picturedcharacter',
+      relatedProperty: 'picturedcharacters',
       dexSelector: 'gallery',
       profileProperty: 'id',
       profileToggle: false,
