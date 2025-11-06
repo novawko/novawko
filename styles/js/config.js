aedad316d69acbba41b0248bab9517da3e7dcbc7
Nova Wolfcreek <novawko@gmail.com>
2025-11-05 20:23:19 -0600

    Update config.js

--- styles/js/config.js
+++ styles/js/config.js
@@ -52,7 +52,7 @@ charadex.sheet = {
     nubreolanSpecies: ['All', 'Kitiguar', 'Fluffy Worm', 'Naa'],
     anatomyTypes: ['All', 'Feral', 'Anthro', 'Humanoid'],
     novawkoArtOnlyToggle: ['Enable'],
-    resourceTypes: ['All', 'Blinkie', 'GIF', 'Illustration', 'Pattern', 'Photo', 'Pixel', 'PNG', 'Stamp', 'Texture', 'Emoji', 'Sticker', 'Moodboard', 'Palette', 'Divider', 'Flag', 'Art Fight'],
+    resourceTypes: ['All', 'Blinkie', 'GIF', 'Illustration', 'Pattern', 'Photo', 'Pixel', 'PNG', 'Stamp', 'Texture', 'Emoji', 'Sticker', 'Moodboard', 'Palette', 'Divider', 'Flag', 'Art Fight', 'Animal Jam', 'Minecraft', 'Pokémon', 'Disability'],
     species: ['All', 'Cat', 'Dog', 'Hamster', 'Kitiguar', 'Human', 'Kanpolo', 'Deep Sea Kanpolo', 'Hyper Kanpolo', 'Kumiyaa', 'Noodle Moth', 'Heavian', 'Burgvoir', 'Sylvarian', 'Woobar', 'Chubbö', 'Yuecoatl', 'Protogen', 'Puffball', 'Yukin', 'Tauyn', 'Eevee', 'Leafeon', 'Sylveon', 'Sprigatito', 'Care Bear', 'Care Bear Cousin', 'Wolf', 'Dragon', 'Rabbit', 'Mecha', 'Fluffy Worm', 'Fox', 'Sparkle Cat', 'Arctic Fox', 'Arctic Wolf', 'Senri', 'Jackalope', 'Flareon', 'Tiger'],
 
   }
@@ -331,8 +331,8 @@ charadex.page.resources = {
   },
 
   filters: {
-    toggle: false,
-    parameters: []
+    toggle: true,
+    parameters: ['All', 'Birthday', 'Halloween', 'Christmas', 'Winter']
   },
 
   fauxFolder: {
