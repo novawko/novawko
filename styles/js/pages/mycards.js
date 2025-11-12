08091be69fa6018a56fca78a7d38d498f13fb6e7
Nova Wolfcreek <novawko@gmail.com>
2025-11-11 22:39:39 -0600

    Update mycards.js

--- styles/js/pages/mycards.js
+++ styles/js/pages/mycards.js
@@ -8,6 +8,6 @@ import { charadex } from '../charadex.js';
 /* Load
 ======================================================================= */
 document.addEventListener("DOMContentLoaded", async () => {
-  let dex = await charadex.initialize.page(null, charadex.page.connections);
+  let dex = await charadex.initialize.page(null, charadex.page.myCards);
   charadex.tools.loadPage('.softload', 500);
 });
