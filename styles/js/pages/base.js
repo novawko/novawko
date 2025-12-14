c872b9c30ed2f1aec9877c05861ffdff3a375466
Nova Wolfcreek <novawko@gmail.com>
2025-12-14 10:08:44 -0600

    Update base.js

--- styles/js/pages/base.js
+++ styles/js/pages/base.js
@@ -44,7 +44,7 @@ $(function() {
 /* ==================================================================== */
 /* Dark Mode Toggle
 ======================================================================= */
-const checkbox = document.getElementById("checkbox")
+const checkbox = document.getElementById("mewwwo")
 checkbox.addEventListener("change", () => {
   document.body.classList.toggle("dark")
 })
