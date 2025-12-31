33bde66830350433ee19961c65cc4f53ba3a0936
Nova Wolfcreek <novawko@gmail.com>
2025-12-31 17:59:04 -0600

    Update favoritecards.js

--- styles/js/pages/favoritecards.js
+++ styles/js/pages/favoritecards.js
@@ -11,15 +11,3 @@ document.addEventListener("DOMContentLoaded", async () => {
   let dex = await charadex.initialize.page(null, charadex.page.favoriteCards);
   charadex.tools.loadPage('.softload', 0);
 });
-
-/* ==================================================================== */
-/* Flipping Card
-======================================================================= */
-// Select the element
-const card = document.querySelector('.flip-card-inner');
-
-// Add a click event listener
-card.addEventListener('click', function() {
-  // Toggle the 'is-flipped' class
-  card.classList.toggle('is-flipped');
-});
