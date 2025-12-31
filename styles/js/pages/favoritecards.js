4dfaec2ce7752e64163ac6f9b52951c89b489f85
Nova Wolfcreek <novawko@gmail.com>
2025-12-31 16:47:09 -0600

    Update favoritecards.js

--- styles/js/pages/favoritecards.js
+++ styles/js/pages/favoritecards.js
@@ -11,3 +11,14 @@ document.addEventListener("DOMContentLoaded", async () => {
   let dex = await charadex.initialize.page(null, charadex.page.favoriteCards);
   charadex.tools.loadPage('.softload', 500);
 });
+
+/* ==================================================================== */
+/* Flipping Card
+======================================================================= */
+var biggercardimages = document.querySelectorAll('.biggercardimage');
+
+[...biggercardimages].forEach((biggercardimage)=>{
+  biggercardimage.addEventListener( 'click', function() {
+    biggercardimage.classList.toggle('is-flipped');
+  });
+});
