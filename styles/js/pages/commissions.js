010c911040cd8a488f94715d745d2848bb263b66
Nova Wolfcreek <novawko@gmail.com>
2026-03-22 09:33:02 -0500

    Update commissions.js

--- styles/js/pages/commissions.js
+++ styles/js/pages/commissions.js
@@ -11,14 +11,3 @@ document.addEventListener("DOMContentLoaded", async () => {
   let dex = await charadex.initialize.page(null, charadex.page.commissions);
   charadex.tools.loadPage('.softload', 500);
 });
-
-/* ==================================================================== */
-/* Remove the Broken Image Icons
-======================================================================= */
-const images = document.querySelectorAll('img');
-
-images.forEach(img => {
-  img.addEventListener('error', function handleError() {
-    this.remove();
-  });
-});
