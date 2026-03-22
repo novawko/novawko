5bcaa4612b473c1c0699b3226b91e06ddefb6dfc
Nova Wolfcreek <novawko@gmail.com>
2026-03-22 09:19:38 -0500

    Update commissions.js

--- styles/js/pages/commissions.js
+++ styles/js/pages/commissions.js
@@ -11,3 +11,14 @@ document.addEventListener("DOMContentLoaded", async () => {
   let dex = await charadex.initialize.page(null, charadex.page.commissions);
   charadex.tools.loadPage('.softload', 500);
 });
+
+/* ==================================================================== */
+/* Remove the Broken Image Icons
+======================================================================= */
+const images = document.querySelectorAll('img');
+
+images.forEach(img => {
+  img.addEventListener('error', function handleError() {
+    this.style.display = 'none';
+  });
+});
