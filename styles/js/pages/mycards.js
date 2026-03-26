a25155c09f4cefdf5d128668591c900e1b983b7e
Nova Wolfcreek <novawko@gmail.com>
2026-03-26 16:50:30 -0500

    Update mycards.js

--- styles/js/pages/mycards.js
+++ styles/js/pages/mycards.js
@@ -8,6 +8,27 @@ import { charadex } from '../charadex.js';
 /* Load
 ======================================================================= */
 document.addEventListener("DOMContentLoaded", async () => {
-  let dex = await charadex.initialize.page(null, charadex.page.myCards);
+
+  let dex = await charadex.initialize.page(
+    null, 
+    charadex.page.myCards, 
+    (arr) => {
+      for (let entry of arr) {
+
+        // Make the tags pretty and actually work
+        entry.tags = entry.tags ? entry.tags.split(',') : [];
+        let fancyTagArr = [];
+        if (entry.tags.length >= 1) {
+          for (let tag of entry.tags) {
+            fancyTagArr.push(`<a href="${charadex.url.addUrlParameters(charadex.url.getPageUrl(charadex.page.imageGallery.sitePage), {tags: tag})}">#${tag.trim()}</a>`);
+          }
+        }
+        entry.fancytags = fancyTagArr.join(' ');
+
+      }
+    }
+  );
+
   charadex.tools.loadPage('.softload', 500);
+
 });
