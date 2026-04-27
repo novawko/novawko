1a3bcdf37b0b6302f862332c49e638583c87beba
Nova Wolfcreek <novawko@gmail.com>
2026-04-27 09:54:43 -0500

    Update prompts.js

--- styles/js/pages/prompts.js
+++ styles/js/pages/prompts.js
@@ -18,6 +18,15 @@ document.addEventListener("DOMContentLoaded", async () => {
         const image = listData.array[i]?.image;
         $(this).attr('style', `background-image: url(${image})`);
       });
+    };
+    let backgroundElement = $('.cd-mewwo-background');
+    if (listData.type == 'profile') {
+      backgroundElement.attr('style', `background-image: url(${listData.profileArray[0].bannerimage})`);
+    } else {
+      backgroundElement.each(function(i) {
+        const bannerimage = listData.array[i]?.bannerimage;
+        $(this).attr('style', `background-image: url(${bannerimage})`);
+      });
     }
   });
   charadex.tools.loadPage('.softload', 500);
