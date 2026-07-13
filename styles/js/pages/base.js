de27d1d2ea9eb0fc925f74ecf8b98c1f751adfc1
Nova Wolfcreek <novawko@gmail.com>
2026-07-12 19:41:32 -0500

    Update base.js

--- styles/js/pages/base.js
+++ styles/js/pages/base.js
@@ -101,6 +101,7 @@ document.addEventListener("DOMContentLoaded", () => {
     transition: "opacity 0.3s ease-in-out",
     width: "50px",
     height: "50px",
+    lineHeight: "0px", // vertically centers the arrow
     boxShadow: "0px 4px 6px rgba(0,0,0,0.5)"
   });
 
