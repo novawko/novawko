f75b12b5efad36b4937f2770246809e1efa3923e
Nova Wolfcreek <novawko@gmail.com>
2026-07-14 02:50:51 -0500

    Update characters.js

--- styles/js/pages/characters.js
+++ styles/js/pages/characters.js
@@ -36,14 +36,14 @@ document.addEventListener("DOMContentLoaded", async () => {
         }
 
         // Make the tags pretty and actually work
-        entry.tags = entry.tags ? entry.tags.split(',') : [];
+        profile.tags = profile.tags ? profile.tags.split(',') : [];
         let fancyTagArr = [];
-        if (entry.tags.length >= 1) {
-          for (let tag of entry.tags) {
+        if (profile.tags.length >= 1) {
+          for (let tag of profile.tags) {
             fancyTagArr.push(`<a href="${charadex.url.addUrlParameters(charadex.url.getPageUrl(charadex.page.characters.sitePage), {tags: tag})}">#${tag.trim()}</a>`);
           }
         }
-        entry.fancytags = fancyTagArr.join(' ');
+        profile.fancytags = fancyTagArr.join(' ');
 
       }
 
