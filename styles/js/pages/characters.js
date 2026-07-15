af5ffc0c9cfc2d01f33d0c37962b13b456f9d950
Nova Wolfcreek <novawko@gmail.com>
2026-07-15 18:27:17 -0500

    Update characters.js

--- styles/js/pages/characters.js
+++ styles/js/pages/characters.js
@@ -2,64 +2,68 @@
 /* Import Charadex
 ======================================================================= */
 import {
-   charadex
-} from '../charadex.js';
+	charadex
+}
+
+from '../charadex.js';
 
 
 /* ==================================================================== */
 /* Load
 ======================================================================= */
-document.addEventListener("DOMContentLoaded", async () => {
+document.addEventListener("DOMContentLoaded", async ()=> {
 
-   let dex = await charadex.initialize.page(
-      null,
-      charadex.page.characters,
-      null,
-      async (listData) => {
+		let dex=await charadex.initialize.page(null,
+			charadex.page.characters,
+			null,
+			async (listData)=> {
 
-         if (listData.type == 'profile') {
+				if (listData.type=='profile') {
 
-            console.log(listData.profileArray[0]);
+					console.log(listData.profileArray[0]);
 
-            // Create the log dex
-            if (charadex.tools.checkArray(listData.profileArray[0].characterlog)) {
-               let logs = await charadex.initialize.page(
-                  listData.profileArray[0].characterlog,
-                  charadex.page.characters.relatedData['character log']
-               );
-            }
+					// Create the log dex
+					if (charadex.tools.checkArray(listData.profileArray[0].characterlog)) {
+						let logs=await charadex.initialize.page(listData.profileArray[0].characterlog,
+							charadex.page.characters.relatedData['character log']);
+					}
 
-            // Create the image gallery
-            if (charadex.tools.checkArray(listData.profileArray[0].imagegallery)) {
-               let gallery = await charadex.initialize.page(
-                  listData.profileArray[0].imagegallery,
-                  charadex.page.characters.relatedData['image gallery']
-               );
-            }
-           
-            // Tags
-            if (profile.tags.length > 0) $("#charadex-profile-tags").show();
+					// Create the image gallery
+					if (charadex.tools.checkArray(listData.profileArray[0].imagegallery)) {
+						let gallery=await charadex.initialize.page(listData.profileArray[0].imagegallery,
+							charadex.page.characters.relatedData['image gallery']);
+					}
 
-         }
+				}
 
-      }
+				// Make the tags pretty and actually work
+				entry.tags=entry.tags ? entry.tags.split(',') : [];
+				let fancyTagArr=[];
 
-   );
+				if (entry.tags.length >=1) {
+					for (let tag of entry.tags) {
+						fancyTagArr.push(`<a href="${charadex.url.addUrlParameters(charadex.url.getPageUrl(charadex.page.imageGallery.sitePage), {tags: tag})}" >#${tag.trim()}</a>`);
+					}
+				}
 
-   charadex.tools.loadPage('.softload', 500);
+				entry.fancytags=fancyTagArr.join(' ');
 
-});
+			});
+
+		charadex.tools.loadPage('.softload', 500);
+
+	});
 
 
 /* ==================================================================== */
 /* Load any specific navtabs
 ======================================================================= */
 $(function () {
-   var hash = window.location.hash;
+		var hash=window.location.hash;
 
-   if (hash != '') {
-      $('.tab-pane').removeClass('active in'); // Remove 'active' from all tab panes
-      $(hash).addClass('active in'); // Add 'active' to the target tab pane
-      $(`a[href="${hash}"]`).tab('show'); // Show the tab linked to the hash
-   }
-});
+		if (hash !='') {
+			$('.tab-pane').removeClass('active in'); // Remove 'active' from all tab panes
+			$(hash).addClass('active in'); // Add 'active' to the target tab pane
+			$(`a[href="${hash}"]`).tab('show'); // Show the tab linked to the hash
+		}
+	});
