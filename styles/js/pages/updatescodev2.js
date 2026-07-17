23117468845c43702821fc68185fbff2b8fb2555
Nova Wolfcreek <novawko@gmail.com>
2026-07-17 09:45:43 -0500

    Create updatescodev2.js

---
+++ styles/js/pages/updatescodev2.js
@@ -0,0 +1,13 @@
+/* ==================================================================== */
+/* Import Charadex
+======================================================================= */
+import { charadex } from '../charadex.js';
+
+
+/* ==================================================================== */
+/* Load
+======================================================================= */
+document.addEventListener("DOMContentLoaded", async () => {
+  let dex = await charadex.initialize.page(null, charadex.page.updatesCodeV2);
+  charadex.tools.loadPage('.softload', 500);
+});
