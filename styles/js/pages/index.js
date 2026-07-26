bcd777d58de75eb0b6eaa590e79e64797215117c
Nova Wolfcreek <novawko@gmail.com>
2026-07-26 03:51:57 -0500

    Update index.js

--- styles/js/pages/index.js
+++ styles/js/pages/index.js
@@ -34,7 +34,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   /* Characters
   ===================================================================== */
-  let designs = await charadex.initialize.page(null, charadex.page.index.ocs, (arr) => {
+  let design = await charadex.initialize.page(null, charadex.page.index.ocs, (arr) => {
 
   // Splice the silly little array
   let sliceAmount = charadex.page.index.ocs.amount || 10;
@@ -45,7 +45,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   /* Updates
   ===================================================================== */
-  let updates = await charadex.initialize.page(null, charadex.page.index.changelog, (arr) => {
+  let update = await charadex.initialize.page(null, charadex.page.index.changelog, (arr) => {
 
   // Splice the silly little array
   let sliceAmount = charadex.page.index.changelog.amount || 20;
