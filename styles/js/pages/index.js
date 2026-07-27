b4bff75be06569e607e701bf68181476e5f8e820
Nova Wolfcreek <novawko@gmail.com>
2026-07-26 22:19:03 -0500

    Update index.js

--- styles/js/pages/index.js
+++ styles/js/pages/index.js
@@ -46,15 +46,18 @@ document.addEventListener("DOMContentLoaded", async () => {
   /* Updates
   ===================================================================== */
   let updates = await charadex.initialize.page(null, charadex.page.index.updates, (arr) => {
-
-  // Splice the silly little array
-  let sliceAmount = charadex.page.index.updates.amount || 10;
-  arr.splice(sliceAmount, arr.length);
-
-  // Reverse the thing
-  let updates = arr.reverse();
     
-  });
+  // Force sliceAmount to 10
+  let sliceAmount = 10;
+
+  // Get the last 10 (or fewer if less than 4 exist)
+  let recent = arr.slice(-sliceAmount);
+
+  // Overwrite original array in-place
+  arr.length = 0;
+  arr.push(...recent);
+    
+});
 
 
   /* Load Page
